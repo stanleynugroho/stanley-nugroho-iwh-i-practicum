13de6d16dcc13e4bb8e8ef44aa9c744110254681
Stanley Nugroho <stanley.nugroho@livingonline.com.au>
2026-02-09 11:27:54 +0800

    Add required routes to the app as per practicum requirements.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 const app = express();
 
 app.set('view engine', 'pug');
@@ -8,19 +9,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const petsUrl = 'https://api.hubapi.com/crm/v3/objects/2-211283277?properties=name,age,type';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        const resp = await axios.get(petsUrl, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Pets | Integrating With HubSpot I Practicum', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update-cobj', async (req, res) => {
+    const newPet = {
+        properties: {
+            "name": req.body.name,
+            "age": req.body.age,
+            "type": req.body.type
+        }
+    };
 
-// * Code for Route 3 goes here
+    const createPetUrl = 'https://api.hubapi.com/crm/v3/objects/2-211283277';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(createPetUrl, newPet, { headers });
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
